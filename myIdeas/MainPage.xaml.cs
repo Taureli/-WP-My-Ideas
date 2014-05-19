162939a50095664a448a1822905fab83f0d9cc00
Taureli <hobkinss@gmail.com>
2014-05-19 14:38:42 +0200

    First commit

---
+++ myIdeas/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using myIdeas.Resources;
+using myIdeas.ViewModels;
+
+namespace myIdeas
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            using(IdeasContext ctx = new IdeasContext(IdeasContext.ConnectionString))
+            {
+                ctx.CreateIfNotExists();
+
+                ctx.LogDebug = true;
+
+                CategoriesList.ItemsSource = ctx.Categories.ToList();
+            }
+        }
+
+        private void Category_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
